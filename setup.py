d5fff6deffcf9aed3fd0d3b637289b9c266d04fa
Simone Pandolfi <simopandolfi@gmail.com>
2019-12-29 16:01:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3.6
-import os
-
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 __author__ = 'Simone Pandolfi <simopandolfi@gmail.com>'
 __version__ = (1, 0, 0)
@@ -9,23 +7,23 @@ __version__ = (1, 0, 0)
 setup(
     name="pyjsonmapper",
     version="0.1",
-    packages=find_packages(),
-    scripts=[filename for filename in os.listdir() if filename.endswith('.py')],
+    description="A simple and hackable mapper from json to python objects... and viceversa!",
+    author="Simone Pandolfi",
+    author_email="simopandolfi@gmail.com",
+    keywords="json mapper",
+    classifiers=[
+        'License :: GNU General Public License v3.0'
+    ],
+    url="https://github.com/simongsr/pyjsonmapper",
+    packages=['distutils', 'distutils.command'],
+    scripts=[
+        '__init__.py',
+        'field.py',
+        'model.py',
+        'validator.py',
+    ],
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-        'hello': ['*.msg'],
     },
-
-    # metadata to display on PyPI
-    author="Simone Pandolfi",
-    author_email="simopandolfi@gmail.com",
-    description="A simple and hackable mapper from json to python objects... and viceversa!",
-    keywords="json mapper",
-    url="https://github.com/simongsr/pyjsonmapper",
-    project_urls={},
-    classifiers=[
-        'License :: GNU General Public License v3.0'
-    ]
 )
