9dff39f5737e342ba2e50f72882e5e9391f96ecb
Simone Pandolfi <simopandolfi@gmail.com>
2019-12-29 17:29:39 +0100

    Update packages

--- __init__.py
+++ __init__.py
@@ -3,3 +3,6 @@
 __author__ = 'Simone Pandolfi <simopandolfi@gmail.com>'
 __version__ = (1, 0, 0)
 
+from pyjsonmapper import field
+from pyjsonmapper import model
+from pyjsonmapper import validator
