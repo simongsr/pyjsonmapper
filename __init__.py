5c6c3f4d052850125794a138e657ac5eb0c32a0d
Simone Pandolfi <simopandolfi@gmail.com>
2020-02-07 12:35:27 +0100

    Update

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3.6
-
-__author__ = 'Simone Pandolfi <simopandolfi@gmail.com>'
-__version__ = (1, 0, 0)
-
-from pyjsonmapper import field
-from pyjsonmapper import model
-from pyjsonmapper import validator
